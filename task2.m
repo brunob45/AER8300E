16e157983dbe3c1a0c813bf49ac9b814496b093c
Hassan Fouad <hafoua@gigl.polymtl.ca>
2020-03-02 11:11:06 -0500

    fini task2

--- task2.m
+++ task2.m
@@ -15,22 +15,41 @@ scenario.StopTime = '19 Feb 2021 00:00:00.000';
 root.ExecuteCommand('Animate * Reset');
 
 %% Montreal
-facility = scenario.Children.New('eFacility','Montreal');
-facility.Position.AssignGeodetic(45.5889,-73.5616,0);
+montreal = scenario.Children.New('eFacility','Montreal');
+montreal.Position.AssignGeodetic(45.5889,-73.5616,0);
 
 %% Satellite1
-scenario.Children.New('eSatellite','SunSat1');
+sat1 = scenario.Children.New('eSatellite','SunSat1');
 root.ExecuteCommand('OrbitWizard */Satellite/SunSat1 SunSynchronous Altitude 901000 LocalTimeAscNode 20:00:00.000');
 
 %% Satellite2
-scenario.Children.New('eSatellite','SunSat2');
+sat2 = scenario.Children.New('eSatellite','SunSat2');
 root.ExecuteCommand('OrbitWizard */Satellite/SunSat2 SunSynchronous Altitude 1688000 LocalTimeAscNode 18:15:00.000');
 
 %% Satellite3
-scenario.Children.New('eSatellite','SunSat3');
+sat3 = scenario.Children.New('eSatellite','SunSat3');
 root.ExecuteCommand('OrbitWizard */Satellite/SunSat3 SunSynchronous Altitude 1200000 LocalTimeAscNode 12:00:00.000');
 
+%% sat1 access
+access = sat1.GetAccessToObject(montreal);
+access.ComputeAccess;
+accessDP = access.DataProviders.Item('Access Data').Exec(scenario.StartTime,scenario.StopTime);
+accessDur1 = accessDP.DataSets.GetDataSetByName('Duration').GetValues;
+sum([accessDur1{:}])/3600
 
+%% sat2 access
+access = sat2.GetAccessToObject(montreal);
+access.ComputeAccess;
+accessDP = access.DataProviders.Item('Access Data').Exec(scenario.StartTime,scenario.StopTime);
+accessDur2 = accessDP.DataSets.GetDataSetByName('Duration').GetValues;
+sum([accessDur2{:}])/3600
+
+%% sat3 access
+access = sat3.GetAccessToObject(montreal);
+access.ComputeAccess;
+accessDP = access.DataProviders.Item('Access Data').Exec(scenario.StartTime,scenario.StopTime);
+accessDur3 = accessDP.DataSets.GetDataSetByName('Duration').GetValues;
+sum([accessDur3{:}])/3600
 
 
 
